955b8328f3c17a6ddbbaf6118aeef14258660745
ma9da <dodymasry25@gmail.com>
2024-11-16 21:55:15 +0200

    added home final structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { StatisticsCardComponent } from './components/statistics-card/statistics-card.component';
+import { StationsListComponent } from './components/stations-list/stations-list.component';
+import { StationsSalesListComponent } from './components/stations-sales-list/stations-sales-list.component';
+import { StationRadarChartComponent } from './components/station-radar-chart/station-radar-chart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, NavbarComponent],
+  imports: [
+    SidebarComponent,
+    NavbarComponent,
+    StatisticsCardComponent,
+    StationRadarChartComponent,
+    StationsListComponent,
+    StationsSalesListComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

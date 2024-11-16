c31016d13be98b7d49d0325120dea43f042ee642
ma9da <dodymasry25@gmail.com>
2024-11-16 21:52:16 +0200

    create sales list component

---
+++ src/app/models/istation.ts
@@ -0,0 +1,10 @@
+export interface Istation {
+  stationName: string;
+  avatarClass: string;
+  totalSales: number;
+  totalValue: number;
+  compressionState: string;
+  totalTransactionCount: number;
+  maxTransHrs: number;
+  maxTransTime: string;
+}
